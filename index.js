dc9910f394ebfeeab3d2e12425667b3b32843c4c
nathan hains <nathanhains14@yahoo.com>
2021-05-17 23:25:49 -0400

    commit tree 081621c840b39ee9f92548eb02bc23960a89e13a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(sourceArray, value){
+    let newArray = []
+    sourceArray.forEach(a => newArray.push(value(a)))
+    return newArray
+}
+
+function reduce(arr, callback, start) {
+    let accumulator = start ? start : arr[0];
+    let i = start ? 0 : 1;
+  
+    for (; i < arr.length; i++) {
+      accumulator = callback(arr[i], accumulator);
+    }
+  
+    return accumulator;
+  }
